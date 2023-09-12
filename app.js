d8b596d194af5eb13be785ee2d2d5d929a3b290a
Julio <93440453+Middagh@users.noreply.github.com>
2023-09-12 01:43:06 +0000

    first attempt

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("Hello, World!");
+const h1Element = document.querySelector("h1");
+
+h1Element.addEventListener("click", function() {
+    alert("¡Hola desde el evento onclick!");
+});
+
